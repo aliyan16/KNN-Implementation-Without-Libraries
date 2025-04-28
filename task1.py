4995437841f1a02f5330dac16c5dd18c74693089
aliyan16 <aliyanm12376@gmail.com>
2025-04-28 23:50:35 +0500

    full implementation

--- task1.py
+++ task1.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import string
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 def readData(path):
     try:
@@ -13,7 +15,8 @@ def readData(path):
 
 
 def calculateDistance(instance1,instance2):
-    for k in range(4):
+    distance=0
+    for k in range(3):
         distance+=instance2[k]-instance1[k]
     
     return np.sqrt(distance)
@@ -24,15 +27,15 @@ def trainTestDistance(trainData,testData):
     l=4
     euclidean=[]
     feature=[]
-    for i in range(testData):
-        for j in range(trainData):
+    for i in range(len(testData)):
+        for j in range(len(trainData)):
             distance=calculateDistance(testData[i][1:l],trainData[j][1:l])
             euclidean.append(distance)
-            labels.append(trainData[5])
+            labels.append(trainData[j][5])
         feature.append(euclidean)
     return feature,labels
 
-def kkn(k,feature,labels):
+def knn(k,feature,labels):
     samplesLength=len(feature)
     distancesLength=len(feature[0])
     knnFeatures=[]
@@ -57,26 +60,35 @@ def kkn(k,feature,labels):
 def getResponse(knnFeatures,knnLabels):
     trainDistances=len(knnFeatures)
     neighbors=len(knnFeatures[0])
-    mostOccuring=[]
-    CounterOccuring=[]
+    NewLabels=[]
     for i in range(trainDistances):
+        mostOccuring=[]
+        CounterOccuring=[]
+        maxValue=0
+        maxIndex=0
         for j in range(neighbors):
             count=0
             clabel=knnLabels[i][j]
-            for k in range(neighbors-1):
-                if clabel in mostOccuring:
-                    
-                if knnLabels[i][(j+k)%neighbors]==clabel:
-                    count+=1
-                if clabel not in mostOccuring:
-                    mostOccuring.append(clabel)
-            
-            CounterOccuring.append(count)
-            
-                       
-            
+            if clabel not in mostOccuring:
+                for k in range(neighbors-1):
+                    if knnLabels[i][(j+k)%neighbors]==clabel:
+                        count+=1
+                    if clabel not in mostOccuring:
+                        mostOccuring.append(clabel)
+                CounterOccuring.append(count)
+        for idx in range(1, len(CounterOccuring)):
+            if CounterOccuring[idx] > maxValue:
+                maxValue = CounterOccuring[idx]
+                maxIndex = idx
+        NewLabels.append(knnLabels[i][maxIndex])
 
-    return
+    return NewLabels
+def getYtest(testData):
+    ytest=[]
+    for i in range(len(testData)):
+        ytest.append(testData[i][5])
+    return ytest
+
 
 
 if __name__=='__main__':
@@ -87,7 +99,17 @@ if __name__=='__main__':
     index2=index1+20
     trainData=data[0:index1]
     testData=data[index2:length]
-    print('here',trainData[0])
-    print(len(trainData))
-    print(len(testData))
+    # print('here',trainData[0])
+    # print(len(trainData))
+    # print(len(testData))
+    ytest=getYtest(testData)
+    feature,labels=trainTestDistance(trainData,testData)
+    knnFeatures,knnLabels=knn(3,feature,labels)
+    PredictedLabels=getResponse(knnFeatures,knnLabels)
+    # cm = confusion_matrix(ytest, PredictedLabels) 
+    cm = confusion_matrix(ytest, PredictedLabels, labels=np.unique(ytest)) 
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=ytest) 
+    disp.plot() 
+    plt.show() 
+
 
